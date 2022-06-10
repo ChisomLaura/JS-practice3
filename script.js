633598b54ebe40202ea320f0f6d32a14739c7eea
Chisom Laura <102640478+ChisomLaura@users.noreply.github.com>
2022-06-10 09:24:54 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var hourlyWage = prompt("How much money do you make an hour? (ex $45)");
+var weeklyHours = prompt("How many hours do you work in a week? (ex 20)");
+
+console.log(
+  `The total amount of money made in a week is $${hourlyWage * weeklyHours}.`
+);
